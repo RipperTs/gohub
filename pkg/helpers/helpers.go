ebfa53533d8a14fabd10f433cf33d5561222e9d5
ripperts <wyfabab123>
2022-06-12 11:46:48 +0800

    验证码库

--- pkg/helpers/helpers.go
+++ pkg/helpers/helpers.go
@@ -2,8 +2,10 @@
 package helpers
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
+	"io"
 	"reflect"
 	"time"
 )
@@ -44,4 +46,18 @@ func MapToJson(param map[string][]string) string {
 	dataType, _ := json.Marshal(param)
 	dataString := string(dataType)
 	return dataString
+}
+
+// RandomNumber 生成长度为 length 随机数字字符串
+func RandomNumber(length int) string {
+	table := [...]byte{'1', '2', '3', '4', '5', '6', '7', '8', '9', '0'}
+	b := make([]byte, length)
+	n, err := io.ReadAtLeast(rand.Reader, b, length)
+	if n != length {
+		panic(err)
+	}
+	for i := 0; i < len(b); i++ {
+		b[i] = table[int(b[i])%len(table)]
+	}
+	return string(b)
 }
