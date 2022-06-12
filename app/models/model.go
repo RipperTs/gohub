94114992ef9fa61aa6c799d44cb3a25cc363b8b7
ripperts <wyfabab123>
2022-06-12 19:36:03 +0800

    软删除

--- app/models/model.go
+++ app/models/model.go
@@ -3,18 +3,21 @@ package models
 
 import (
 	"github.com/spf13/cast"
+	"gorm.io/gorm"
 	"time"
 )
 
 // BaseModel 模型基类
 type BaseModel struct {
-	ID uint64 `gorm:"column:id;primaryKey;autoIncrement;" json:"id,omitempty"`
+	ID        uint64 `gorm:"column:id;primaryKey;autoIncrement;" json:"id,omitempty"`
+	DeletedAt gorm.DeletedAt
 }
 
 // CommonTimestampsField 时间戳
 type CommonTimestampsField struct {
 	CreatedAt time.Time `gorm:"column:created_at;index;" json:"created_at,omitempty"`
 	UpdatedAt time.Time `gorm:"column:updated_at;index;" json:"updated_at,omitempty"`
+	DeletedAt time.Time `gorm:"column:deleted_at;index" json:"deleted_at,omitempty"`
 }
 
 // GetStringID 获取 ID 的字符串格式
