727c5a1fa3170c7a26400839f6ca9bbb17cf84c4
ripperts <wyfabab123>
2022-06-12 14:01:34 +0800

    路由调整

--- routes/api.go
+++ routes/api.go
@@ -10,18 +10,15 @@ import (
 // RegisterAPIRoutes 注册API路由
 func RegisterAPIRoutes(r *gin.Engine) {
 
+	r.GET("/", func(c *gin.Context) {
+		// 以 JSON 格式响应
+		c.JSON(http.StatusOK, gin.H{
+			"msg": "Hello GoHub!",
+		})
+	})
 	// 所有的 v1 版本的路由都将存放到这里
 	v1 := r.Group("/v1")
 	{
-		// 注册一个路由
-		v1.GET("/", func(c *gin.Context) {
-			// 以 JSON 格式响应
-			c.JSON(http.StatusOK, gin.H{
-				"Hello": "GoHub",
-			})
-		})
-
-		// v1通用路由组
 		authGroup := v1.Group("/auth")
 		{
 			suc := new(auth.SignupController)
