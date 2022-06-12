e21312444ed5df548b1d3f38bd925b9df79c4a22
ripperts <wyfabab123>
2022-06-12 08:37:22 +0800

    初始化日志驱动

--- main.go
+++ main.go
@@ -21,7 +21,12 @@ func main() {
 	var env string
 	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
 	flag.Parse()
+
+	// 加载配置文件
 	config.InitConfig(env)
+
+	// 初始化 Logger
+	bootstrap.SetupLogger()
 
 	// new 一个 Gin Engine 实例
 	router := gin.New()
